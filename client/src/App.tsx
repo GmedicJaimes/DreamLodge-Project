2c1597339ac66fc5c7827bf87a41db25be7fd747
GullianoJaimes <akiloty3@gmail.com>
2023-08-02 10:07:40 -0500

    modificacion de componentes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,16 +1,15 @@
 import { Routes, Route } from 'react-router-dom';
-import Landing from './views/Landing/LandingPage'
+import Landing from './views/Landing/LandingPage';
 
+import { HomePage } from './views/Home/HomePage';
 import './App.css'
-import { HomePage } from './views/HomePage'
 
 const App = (): JSX.Element => {
   return (
-    
     <Routes>
       <Route path='/' element={<Landing/>}/>
       <Route path='/home' element={<HomePage/>}/>
-      <Route path='/home/:id' element={''}/>
+      <Route path='/home/:id' element={''}/> 
       <Route path='/create' element={''}/>
     </Routes>
   )
--- client/src/views/Landing/LandingPage.tsx
+++ client/src/views/Landing/LandingPage.tsx
@@ -2,7 +2,8 @@ import { Link } from 'react-router-dom';
 
 import style from './Landing.module.css'
 
-const Landing = () => {
+const Landing = (): JSX.Element  => {
+
   return (
     <div>
       <h1 className={style.h1}>Estamos en la Landing</h1>
