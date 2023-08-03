ceba27bd92c7409807e9ea28278f0ab16c43ef58
Joaquin Acosta <joaquinacosta397@outlook.com>
2023-08-02 23:01:08 -0300

    fakedetail

--- client/src/views/Detail/DetailPost/DetailPost.jsx
+++ client/src/views/Detail/DetailPost/DetailPost.jsx
@@ -1,7 +1,56 @@
 const DetailPost = () => {
     return(
         <div>
-            
+            <div>
+                <h1>Depto</h1>
+                <h2>Precio</h2>
+                <p>Departamento lindo blablabla ubicado frente al mar etc</p>
+                <button>Reserve</button>
+            </div>
+            <div>
+                <img src="" alt="" />
+            </div>
+            <div>
+                <div>
+                    <h2>Overview</h2>
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magni enim nostrum, neque laudantium culpa maiores pariatur dolorem possimus officia amet sint quisquam vel, tenetur, cum cupiditate consequatur? Consequuntur, enim blanditiis.</p>
+                </div>
+                <div>
+                    <h2>Rating</h2>
+                    <p>4,20</p>
+                </div>
+            </div>
+            <div>
+                <h2>Services</h2>
+                <div>
+                    <ul>
+                        <li>Restaurant</li>
+                        <li>Cafeteria</li>
+                        <li>Wifi</li>
+                        <li>TV</li>
+                        <li>Bathroom</li>
+                        <li>Safety deposit box</li>
+                        <li>Garage</li>
+                        <li>Room Service</li>
+                        <li>Pets allowed</li>
+                        <li>Heating</li>
+                        <li>Washer</li>
+                        <li>Kitchen</li>
+                    </ul>
+                </div>
+            </div>
+            <div>
+                <h2>Opinions</h2>
+                <div>
+                    <ul>
+                        <li>Nice</li>
+                        <li>Bad</li>
+                        <li>xD</li>
+                        <li>:v</li>
+                        <li>:'v</li>
+                    </ul>
+                </div>
+            </div>
         </div>
     )
 }
