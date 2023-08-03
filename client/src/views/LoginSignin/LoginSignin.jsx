5b4487d25c1e11133834f57eacec75b3ccbd7d2a
dannagonim <dannagonim@gmail.com>
2023-08-03 16:11:25 -0300

    componente, login, singin, searchbar

---
+++ client/src/views/LoginSignin/LoginSignin.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Login from '../../components/Navbar/User/Login/Login'
+
+export const LoginSignin = () => {
+  return (
+    <div>
+        <Login/>
+    </div>
+  )
+}
+
+
