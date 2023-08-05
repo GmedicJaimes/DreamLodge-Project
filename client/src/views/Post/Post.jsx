0289844768b1b50a6ee178a5c741db47797868c0
Joaquin Acosta <joaquinacosta397@outlook.com>
2023-08-05 14:42:18 -0300

    formulario 2

--- client/src/views/Post/Post.jsx
+++ client/src/views/Post/Post.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import styles from './post.module.css';
+import About from '../../components/About/About';
 
 export const Post = () => {
   const dispatch = useDispatch();
@@ -13,6 +14,8 @@ export const Post = () => {
     description: '',
     price: ''
   });
+
+  const opciones = [0, 1, 2, 3, 4, 5, 6]
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -29,100 +32,140 @@ export const Post = () => {
   };
 
   return (
-    <form onSubmit={handleSubmit} className={styles.mainContainer}>
-      <header>
-        <h2>Create your account</h2>
-      </header>
-      <div className={styles.formGroup}>
-        <label>
-          Name:
-          <input type="text" name="name" value={formData.name} onChange={handleChange} />
-        </label>
-      </div>
-      <div className={styles.formGroup}>
-        <label>
-          Tipo:
-          <select name="type" value={formData.type} onChange={handleChange}>
-            <option value="cabaña">Cabaña</option>
-            <option value="casa_playa">Casa de Playa</option>
-            {/* Agrega más opciones aquí */}
-          </select>
-        </label>
-      </div>
-      <div className={styles.formGroup}>
-        <h3>Location</h3>
-        <label>
-          Country:
-          <input type="text" name="location" value={formData.location} onChange={handleChange} />
-        </label>
-      </div>
-      <div className={styles.formGroup}>
-        <label>
-          State:
-          <input type="text" name="location" value={formData.location} onChange={handleChange} />
-        </label>
-      </div>
-      <div className={styles.formGroup}>
-        <label>
-          Direction:
-          <input type="text" name="location" value={formData.location} onChange={handleChange} />
-        </label>
-      </div>
-      <div className={styles.formGroup}>
-        <h3>Rooms</h3>
-        <label>
-        Travellers:
-          <input type="text" name="location" value={formData.location} onChange={handleChange} />
-        </label>
-      </div>
-      <div className={styles.formGroup}>
-        <label>
-          rooms:
-          <input type="text" name="location" value={formData.location} onChange={handleChange} />
-        </label>
-      </div>
-      <div className={styles.formGroup}>
-        <label>
-          Bathrooms:
-          <input type="text" name="location" value={formData.location} onChange={handleChange} />
-        </label>
-      </div>
-      <div className={styles.formGroup}>
-        <label>
-          Beds:
-          <input type="text" name="location" value={formData.location} onChange={handleChange} />
-        </label>
-      </div>
-      <div className={styles.formGroup}>
-        <label>
-          Services:
-          <select name="services" value={formData.services} onChange={handleChange}>
-            <option value="wifi">Wifi</option>
-            <option value="piscina">Piscina</option>
-            {/* Agrega más opciones aquí */}
-          </select>
-        </label>
-      </div>
-      <div className={styles.formGroup}>
-        <label>
-          Image:
-          <input type="text" name="image" value={formData.image} onChange={handleChange} />
-        </label>
-      </div>
-      <div className={styles.formGroup}>
-        <label>
-          Description:
-          <textarea name="description" value={formData.description} onChange={handleChange} />
-        </label>
-      </div>
-      <div className={styles.formGroup}>
-        <label>
-          Price:
-          <input type="text" name="price" value={formData.price} onChange={handleChange} />
-        </label>
-      </div>
-      <button type="submit">Crear Posteo</button>
-    </form>
+    <div>
+    <div className={styles.bigContainer}>
+      <form onSubmit={handleSubmit} className={styles.mainContainer}>
+        <header>
+          <h2>Create your account</h2>
+        </header>
+        <div className={styles.formGroup}>
+          <label>
+            Name:
+            <input type="text" name="name" value={formData.name} onChange={handleChange} />
+          </label>
+        </div>
+        <div className={styles.formGroup}>
+          <label>
+            Tipo:
+            <select name="type" value={formData.type} onChange={handleChange}>
+              <option value="cabaña">Cabaña</option>
+              <option value="casa_playa">Casa de Playa</option>
+              {/* Agrega más opciones aquí */}
+            </select>
+          </label>
+        </div>
+        <div className={styles.formGroup}>
+          <h3>Location</h3>
+          <label>
+            Country:
+            <input type="text" name="location" value={formData.location} onChange={handleChange} />
+          </label>
+        </div>
+        <div className={styles.formGroup}>
+          <label>
+            State:
+            <input type="text" name="location" value={formData.location} onChange={handleChange} />
+          </label>
+        </div>
+        <div className={styles.formGroup}>
+          <label>
+            Direction:
+            <input type="text" name="location" value={formData.location} onChange={handleChange} />
+          </label>
+        </div>
+        <div className={styles.formGroup}>
+          <h3>Rooms</h3>
+          <div className={styles.roomsSelects}>
+            <div>
+            <label>
+            Travellers:
+            <select name="" id="" className={styles.opciones}>
+                {
+                  opciones.map((opt)=>{
+                    return(
+                      <option value={opt}>{opt}</option>
+                    )
+                  })
+                }
+              </select>
+            </label>
+          </div>
+          <div className={styles.formGroup}>
+            <label>
+              rooms:
+              <select name="" id="">
+                {
+                  opciones.map((opt)=>{
+                    return(
+                      <option value={opt}>{opt}</option>
+                    )
+                  })
+                }
+              </select>
+            </label>
+          </div>
+          <div className={styles.formGroup}>
+            <label>
+              Bathrooms:
+              <select name="" id="">
+                {
+                  opciones.map((opt)=>{
+                    return(
+                      <option value={opt}>{opt}</option>
+                    )
+                  })
+                }
+              </select>
+            </label>
+          </div>
+          <div className={styles.formGroup}>
+            <label>
+              Beds:
+              <select name="" id="">
+                {
+                  opciones.map((opt)=>{
+                    return(
+                      <option value={opt}>{opt}</option>
+                    )
+                  })
+                }
+              </select>
+            </label>
+          </div>
+          </div>
+        </div>
+        <div className={styles.formGroup}>
+          <label>
+            Services:
+            <select name="services" value={formData.services} onChange={handleChange}>
+              <option value="wifi">Wifi</option>
+              <option value="piscina">Piscina</option>
+              {/* Agrega más opciones aquí */}
+            </select>
+          </label>
+        </div>
+        <div className={styles.formGroup}>
+          <label>
+            Image:
+            <input type="text" name="image" value={formData.image} onChange={handleChange} />
+          </label>
+        </div>
+        <div className={styles.formGroup}>
+          <label>
+            Description:
+            <textarea type="text" name="description" value={formData.description} onChange={handleChange} />
+          </label>
+        </div>
+        <div className={styles.formGroup}>
+          <label>
+            Price:
+            <input className={styles.range} onChange={handleChange} type="range" name="price" value={formData.price}/><p>{formData.price}</p>
+          </label>
+        </div>
+        <button className={styles.btn} type="submit">Crear Posteo</button>
+      </form>
+    </div>
+      <About/>
+    </div>
   );
 };
-
