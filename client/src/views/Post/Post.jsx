ceda514a76897ed8ebae0e12a0f6813653031324
agustinibar <agustinibarperrotta@gmail.com>
2023-08-06 15:34:57 -0300

    errores de sintaxis

--- client/src/views/Post/Post.jsx
+++ client/src/views/Post/Post.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useForm } from 'react-hook-form';
 import { useDispatch } from 'react-redux';
-import { createProperty } from '../redux/actions/propertyActions';
+import { createProperty } from '../../redux/actions';
 
 const Post= () => {
   const dispatch = useDispatch();
