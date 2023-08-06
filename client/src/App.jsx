fa1e935856ba8b15df81b7bcd868f72dcd613df4
Chris <chris@192.168.1.69>
2023-08-06 13:24:04 -0300

    agregue searchbar y cambios en los forms

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,10 +7,10 @@ import Navbar from './components/Navbar/Navbar'
 import DetailPost from "./views/Detail/DetailPost/DetailPost"
 import DetailUser from "./views/Detail/DetaiUser/DetailUser"
 import  {Post}  from './views/Post/Post';
-
+import '@fortawesome/fontawesome-free/css/all.min.css';
 import { useLocation } from 'react-router-dom'
 import { LoginSignin } from './views/LoginSignin/LoginSignin'
-
+import { SignInView } from './views/signing/SignInView';
 
 function App() {
 
@@ -27,6 +27,7 @@ function App() {
         <Route path='/home' element={<Homepage/>}/>
 
         <Route path='/login' element={<LoginSignin/>}/>
+        <Route path='/signin' element={<SignInView/>}/>
         <Route path='/rooms/:id' element={<DetailPost/>}/>
         <Route path='/user/:id' element={<DetailUser/>}/>
         <Route path='/post' element={<Post/>}/>  
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,6 +4,8 @@ import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import store from "./redux/store.js"
+import '@fortawesome/fontawesome-free/css/all.min.css';
+
 
 import './index.css'
 
