946466333eec470e0ede2499ad28f22a2b45ead7
Joaquin Acosta <joaquinacosta397@outlook.com>
2023-08-07 00:46:41 -0300

    cositas

--- client/src/views/Post/Post.jsx
+++ client/src/views/Post/Post.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { createPost } from '../../redux/actions';
+import styles from "./post.module.css"
+import About from "../../components/About/About"
 
 const Post = () => {
   const dispatch = useDispatch();
@@ -35,8 +37,10 @@ const Post = () => {
 
   const onSubmit = async (event) => {
     event.preventDefault();
+    console.log(formData);
     try {
       await dispatch(createPost(formData)); // Enviar el objeto formData completo
+      
       alert('Property created successfully!');
     } catch (error) {
       console.error(error);
@@ -45,45 +49,53 @@ const Post = () => {
   };
 
   return (
-    <form onSubmit={onSubmit}>
-      <div>
-        <label>User ID:</label>
-        <input type="text" name="user_id" value={formData.user_id} onChange={handleInputChange} required />
+    <div>
+      <div className={styles.mainContainer}>
+        <header>
+          <h2>Post your lodge</h2>
+        </header>
+        <form onSubmit={onSubmit}>
+          <div className={styles.formGroup}>
+            <label>User ID:</label>
+            <input type="text" name="user_id" value={formData.user_id} onChange={handleInputChange} required />
+          </div>
+          <div className={styles.formGroup}>
+            <label>Property Name:</label>
+            <input type="text" name="name" value={formData.name} onChange={handleInputChange} required />
+          </div>
+          <div className={styles.formGroup}>
+            <label>Property Types:</label>
+            <input type="text" name="types" value={formData.types} onChange={handleInputChange} required />
+          </div>
+          <div className={styles.formGroup}>
+            <label>Location:</label>
+            <input type="text" name="location" value={formData.location} onChange={handleInputChange} required />
+          </div>
+          <div className={styles.formGroup}>
+            <label>Rooms:</label>
+            <input type="number" name="rooms" value={formData.rooms} onChange={handleInputChange} required />
+          </div>
+          <div className={styles.formGroup}>
+            <label>Services:</label>
+            <input type="text" name="services" value={formData.services} onChange={handleInputChange} required />
+          </div>
+          <div className={styles.formGroup}>
+            <label>Description:</label>
+            <input type="text" name="description" value={formData.description} onChange={handleInputChange} required />
+          </div>
+          <div className={styles.formGroup}>
+            <label>Price:</label>
+            <input type="number" name="price" value={formData.price} onChange={handleInputChange} required />
+          </div>
+          <div className={styles.formGroup}>
+            <label>Image:</label>
+            <input type="file" name="imageFile" onChange={handleFileChange} required />
+          </div>
+          <button className={styles.btn} type="submit">Create Property</button>
+        </form>
       </div>
-      <div>
-        <label>Property Name:</label>
-        <input type="text" name="name" value={formData.name} onChange={handleInputChange} required />
-      </div>
-      <div>
-        <label>Property Types:</label>
-        <input type="text" name="types" value={formData.types} onChange={handleInputChange} required />
-      </div>
-      <div>
-        <label>Location:</label>
-        <input type="text" name="location" value={formData.location} onChange={handleInputChange} required />
-      </div>
-      <div>
-        <label>Rooms:</label>
-        <input type="number" name="rooms" value={formData.rooms} onChange={handleInputChange} required />
-      </div>
-      <div>
-        <label>Services:</label>
-        <input type="text" name="services" value={formData.services} onChange={handleInputChange} required />
-      </div>
-      <div>
-        <label>Description:</label>
-        <input type="text" name="description" value={formData.description} onChange={handleInputChange} required />
-      </div>
-      <div>
-        <label>Price:</label>
-        <input type="number" name="price" value={formData.price} onChange={handleInputChange} required />
-      </div>
-      <div>
-        <label>Image:</label>
-        <input type="file" name="imageFile" onChange={handleFileChange} required />
-      </div>
-      <button type="submit">Create Property</button>
-    </form>
+      <About/>
+    </div>
   );
 };
 
