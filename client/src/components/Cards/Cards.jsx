e17982d32dac8278c5cc8bebf0bfa0058b5c950d
GullianoJaimes <akiloty3@gmail.com>
2023-08-07 16:32:29 -0500

    cambios en pausa en la rama frontend

--- client/src/components/Cards/Cards.jsx
+++ client/src/components/Cards/Cards.jsx
@@ -1,11 +1,18 @@
 
 import Card from "./Card/Card"
 import React from 'react';
-import { useState, useEffect, useRef } from "react";
+// import { useState, useEffect, useRef } from "react";
 import styles from "./Cards.module.css"
+import { useInfiniteQuery } from "@tanstack/react-query";
 
 
 const Cards = ({allProperties}) => {
+
+    useInfiniteQuery(
+      ['property'],
+      () => {}
+    )
+
 
     return(
       <div className={styles.containerCards}>
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -7,14 +7,21 @@ import store from "./redux/store.js"
 import '@fortawesome/fontawesome-free/css/all.min.css';
 
 
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+
 import './index.css'
+
+const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
     <React.StrictMode>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <QueryClientProvider client={queryClient}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </QueryClientProvider>
     </React.StrictMode>
   </Provider>,
 )
