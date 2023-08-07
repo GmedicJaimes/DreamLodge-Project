d3ef6dbdc74e2c3ef312860dbe58affefa93fb13
GullianoJaimes <akiloty3@gmail.com>
2023-08-07 16:52:29 -0500

    cambios

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,16 +3,15 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
-import store from "./redux/store.js"
+
 
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <Provider store={store}>
+
     <React.StrictMode>
       <BrowserRouter>
         <App />
       </BrowserRouter>
     </React.StrictMode>
-  </Provider>,
 )
--- client/src/views/Post/Post.jsx
+++ client/src/views/Post/Post.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
-import { createPost } from '../../redux/actions';
+
 
 const Post = () => {
   const dispatch = useDispatch();
