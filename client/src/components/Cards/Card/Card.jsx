310713fce00fb8ab7706ac9b8f907c2bc2cc50c6
GullianoJaimes <akiloty3@gmail.com>
2023-08-08 18:52:31 -0500

    commit de cambios

--- client/src/components/Cards/Card/Card.jsx
+++ client/src/components/Cards/Card/Card.jsx
@@ -9,7 +9,7 @@ import imagen3 from '../../../assets/imagen3.jpeg';
 import imagen4 from '../../../assets/imagen4.jpeg';
 
 const Card = ({ property }) => {
-  const { id, name, rooms, location, userId , imageUrl} = property;
+  const { id, name, rooms, location, userId , imageUrl, price} = property;
   // console.log(id,name,rooms,location)
   const imageUrls = [landingImg, FondoLanding, imagen1, imagen2, imagen3, imagen4];
 
@@ -24,12 +24,12 @@ const Card = ({ property }) => {
           <img src={imageUrl} alt="pic of the house" />
         </div>
         <section className={styles.info}>
-          {/* <h3>{location.state}, {location.city}</h3> */}
-          <h3>{location}</h3>
+          <h3>{location.state}, {location.city}</h3>
+          {/* <h3>{location}</h3> */}
           {/* <p>{id}</p>
           <p>{userId}</p> */}
           <p className={styles.infoName}>{name}</p>
-          <p className={styles.infoPrice}>$ {rooms} USD noche</p>
+          <p className={styles.infoPrice}>$ {price} USD noche</p>
         </section>
       </div>
     </Link>
