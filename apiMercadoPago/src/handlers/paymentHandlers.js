c18b8dd36ce1a3272c9676678f9d7769e6a08335
Gulliano Jaimes <88950788+GmedicJaimes@users.noreply.github.com>
2023-08-10 20:41:39 -0500

    commit prueba

--- apiMercadoPago/src/handlers/paymentHandlers.js
+++ apiMercadoPago/src/handlers/paymentHandlers.js
@@ -17,7 +17,7 @@ const createPayment = async(req, res)=>{
               },
             ],
             back_urls: {
-                success:"https://dreamlodgeprueba.web.app/home",
+                success:"https://dreamlodgeprueba.web.app/",
                 failure: "http://localhost:3001/failure",
             },
             auto_return: "approved"
