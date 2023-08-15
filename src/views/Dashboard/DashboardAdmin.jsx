663fad1c953dd979564052bc9e4ea9df00e4fe64
agustinibar <agustinibarperrotta@gmail.com>
2023-08-15 19:05:50 -0300

    aceptedPay

--- src/views/Dashboard/DashboardAdmin.jsx
+++ src/views/Dashboard/DashboardAdmin.jsx
@@ -1,42 +1,43 @@
-import React, { useEffect, useState } from 'react';
-import {collection, getDocs} from 'firebase/firestore';
-import {db} from '../../config/firebase';
+  import React, { useEffect, useState } from 'react';
+  import {collection, getDocs} from 'firebase/firestore';
+  import {db} from '../../config/firebase';
 
-const DashboardAdmin = ({totalImages, totalProperties, totalUsers}) => {
-    const [purchaseSize, setPurchaseSize] = useState(0)
-  useEffect(()=>{
-    const getPurchaseCount = async()=>{
-      try {
-        const purchasesSnapshot = await getDocs(collection(db, 'purchases' ));
-        const totalPurchases = purchasesSnapshot.size;
-        setPurchaseSize(totalPurchases)
-      } catch (error) {
-        console.error(error)
+  const DashboardAdmin = ({totalImages, totalProperties, totalUsers}) => {
+      const [purchaseSize, setPurchaseSize] = useState(0);
+      
+    useEffect(()=>{
+      const getPurchaseCount = async()=>{
+        try {
+          const purchasesSnapshot = await getDocs(collection(db, 'purchases' ));
+          const totalPurchases = purchasesSnapshot.size;
+          setPurchaseSize(totalPurchases)
+        } catch (error) {
+          console.error(error)
+        }
       }
-    }
-    getPurchaseCount()
-  }, []);
+      getPurchaseCount()
+    }, []);
 
-  return (
-    <div className="dashboard">
-      <div className="card">
-        <h2>Total de Propiedades</h2>
-        <p>{totalProperties ? totalProperties : "No hay propiedades en la base de datos"}</p>
+    return (
+      <div>
+        <div>
+          <h2>Total de Propiedades</h2>
+          <p>{totalProperties ? totalProperties : "No hay propiedades en la base de datos"}</p>
+        </div>
+        <div>
+          <h2>Usuarios Registrados</h2>
+          <p>{totalUsers ? totalUsers : "No hay usuarios en al base de datos"}</p>
+        </div>
+        <div>
+          <h2>Imagenes cargadas</h2>
+          <p>{totalImages ? totalImages : "No hay imagenes en la base de datos"}</p>
+        </div>
+        <div>
+          <h2>Propiedades Rentadas</h2>
+          <p>{purchaseSize ? purchaseSize : "No se han rentado propiedades aun"}</p>
+        </div>
       </div>
-      <div className="card">
-        <h2>Usuarios Registrados</h2>
-        <p>{totalUsers ? totalUsers : "No hay usuarios en al base de datos"}</p>
-      </div>
-      <div className="card">
-        <h2>Imagenes cargadas</h2>
-        <p>{totalImages ? totalImages : "No hay imagenes en la base de datos"}</p>
-      </div>
-      <div className="card">
-        <h2>Propiedades Rentadas</h2>
-        <p>{purchaseSize ? purchaseSize : "No se han rentado propiedades aun"}</p>
-      </div>
-    </div>
-  );
-}
+    );
+  }
 
-export default DashboardAdmin;
+  export default DashboardAdmin;
