427ff3431cabfebe79a769841af2d8a112dcdec6
agustinibar <agustinibarperrotta@gmail.com>
2023-08-18 11:02:01 -0300

    no fucking fighting

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -6,14 +6,23 @@ import {getFirestore} from 'firebase/firestore';
 import {getStorage} from 'firebase/storage';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDCEXGxA5qGuDPp53bJDgHnr-ARv7s4A7s",
-  authDomain: "dreamlodgeprueba.firebaseapp.com",
-  projectId: "dreamlodgeprueba",
-  storageBucket: "dreamlodgeprueba.appspot.com",
-  messagingSenderId: "937226421893",
-  appId: "1:937226421893:web:13d4f51541cebda5bb8bec",
-  measurementId: "G-6TB25EM3W8"
+  apiKey: "AIzaSyC_YrDe0cR-pUS5vk_QvZ6Z28f_lylJCJ8",
+  authDomain: "pruebadeploy-8f459.firebaseapp.com",
+  projectId: "pruebadeploy-8f459",
+  storageBucket: "pruebadeploy-8f459.appspot.com",
+  messagingSenderId: "191809447425",
+  appId: "1:191809447425:web:30b9622277fc63f85b4200",
+  measurementId: "G-KK3JWKX9LE"
 };
+// const firebaseConfig = {
+//   apiKey: "AIzaSyDCEXGxA5qGuDPp53bJDgHnr-ARv7s4A7s",
+//   authDomain: "dreamlodgeprueba.firebaseapp.com",
+//   projectId: "dreamlodgeprueba",
+//   storageBucket: "dreamlodgeprueba.appspot.com",
+//   messagingSenderId: "937226421893",
+//   appId: "1:937226421893:web:13d4f51541cebda5bb8bec",
+//   measurementId: "G-6TB25EM3W8"
+// };
 
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
