5a490d26453969f87fabbb5ed1cb23ac6ac25369
Joaquin Acosta <joaquinacosta397@outlook.com>
2023-08-19 14:37:15 -0300

    responsive configAcc

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,3 +1,39 @@
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+// import {getAuth, GoogleAuthProvider, FacebookAuthProvider} from 'firebase/auth';
+// import {getFirestore} from 'firebase/firestore';
+// import {getStorage} from 'firebase/storage';
+
+// const firebaseConfig = {
+//   apiKey: "AIzaSyC_YrDe0cR-pUS5vk_QvZ6Z28f_lylJCJ8",
+//   authDomain: "pruebadeploy-8f459.firebaseapp.com",
+//   projectId: "pruebadeploy-8f459",
+//   storageBucket: "pruebadeploy-8f459.appspot.com",
+//   messagingSenderId: "191809447425",
+//   appId: "1:191809447425:web:30b9622277fc63f85b4200",
+//   measurementId: "G-KK3JWKX9LE"
+// };
+// // const firebaseConfig = {
+// //   apiKey: "AIzaSyDCEXGxA5qGuDPp53bJDgHnr-ARv7s4A7s",
+// //   authDomain: "dreamlodgeprueba.firebaseapp.com",
+// //   projectId: "dreamlodgeprueba",
+// //   storageBucket: "dreamlodgeprueba.appspot.com",
+// //   messagingSenderId: "937226421893",
+// //   appId: "1:937226421893:web:13d4f51541cebda5bb8bec",
+// //   measurementId: "G-6TB25EM3W8"
+// // };
+
+
+
+// const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
+// export const auth = getAuth(app);
+// export const googleProvider = new GoogleAuthProvider();
+// export const db = getFirestore(app)
+// export const storage = getStorage(app);
+// export const  facebookProvider = new FacebookAuthProvider()
+
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
@@ -5,15 +41,15 @@ import {getAuth, GoogleAuthProvider, FacebookAuthProvider} from 'firebase/auth';
 import {getFirestore} from 'firebase/firestore';
 import {getStorage} from 'firebase/storage';
 
-const firebaseConfig = {
-  apiKey: "AIzaSyC_YrDe0cR-pUS5vk_QvZ6Z28f_lylJCJ8",
-  authDomain: "pruebadeploy-8f459.firebaseapp.com",
-  projectId: "pruebadeploy-8f459",
-  storageBucket: "pruebadeploy-8f459.appspot.com",
-  messagingSenderId: "191809447425",
-  appId: "1:191809447425:web:30b9622277fc63f85b4200",
-  measurementId: "G-KK3JWKX9LE"
-};
+// const firebaseConfig = {
+//   apiKey: "AIzaSyC_YrDe0cR-pUS5vk_QvZ6Z28f_lylJCJ8",
+//   authDomain: "pruebadeploy-8f459.firebaseapp.com",
+//   projectId: "pruebadeploy-8f459",
+//   storageBucket: "pruebadeploy-8f459.appspot.com",
+//   messagingSenderId: "191809447425",
+//   appId: "1:191809447425:web:30b9622277fc63f85b4200",
+//   measurementId: "G-KK3JWKX9LE"
+// };
 // const firebaseConfig = {
 //   apiKey: "AIzaSyDCEXGxA5qGuDPp53bJDgHnr-ARv7s4A7s",
 //   authDomain: "dreamlodgeprueba.firebaseapp.com",
@@ -23,6 +59,15 @@ const firebaseConfig = {
 //   appId: "1:937226421893:web:13d4f51541cebda5bb8bec",
 //   measurementId: "G-6TB25EM3W8"
 // };
+const firebaseConfig = {
+  apiKey: "AIzaSyC-Bb1urfKlXTTVkPTgO-gCcsHKaHWtloM",
+  authDomain: "foodiesitelab-99c62.firebaseapp.com",
+  projectId: "foodiesitelab-99c62",
+  storageBucket: "foodiesitelab-99c62.appspot.com",
+  messagingSenderId: "195800164351",
+  appId: "1:195800164351:web:74dbc6e62e9282c9cca9ca",
+  measurementId: "G-XG83CW4362"
+};
 
 
 
@@ -30,7 +75,6 @@ const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 export const auth = getAuth(app);
 export const googleProvider = new GoogleAuthProvider();
-export const db = getFirestore(app)
+export const db = getFirestore(app);
 export const storage = getStorage(app);
-export const  facebookProvider = new FacebookAuthProvider()
-
+export const  facebookProvider = new FacebookAuthProvider();
